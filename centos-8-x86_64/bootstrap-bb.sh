7c3a79e36fcb5a33c20b2bf40d5762bea6ae5654
Bruno Travouillon <devel@travouillon.fr>
2020-01-28 08:35:09 -0500

    ci: add Vagrantfile to test simple_cluster
    Also add the bootstrap-bb.sh script to provision the management node
    with Vagrant. This script setup the environment to run the bluebanquise
    stack.

---
+++ centos-8-x86_64/bootstrap-bb.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+set -x
+
+echo "Install packages"
+/usr/bin/dnf install epel-release git wget redhat-lsb-core -y
+/usr/bin/dnf install ansible -y
+
+DISTRIBUTION=$(lsb_release -is|tr '[:upper:]' '[:lower:]')
+RELEASE=$(lsb_release -rs)
+MAJOR_RELEASE=${RELEASE:0:1}
+MINOR_RELEASE=${RELEASE:0:3}
+ARCH=$(uname -i)
+
+echo "Install BlueBanquise"
+/usr/bin/git clone https://github.com/oxedions/bluebanquise /etc/bluebanquise
+
+echo "Setup the simple_cluster environment"
+# os repository
+/usr/bin/mkdir -p /var/www/html/repositories/${DISTRIBUTION}/${MINOR_RELEASE}/${ARCH}/os/
+/usr/bin/mount /dev/cdrom /var/www/html/repositories/${DISTRIBUTION}/${MINOR_RELEASE}/${ARCH}/os/
+
+# Download bluebanquise repository if we don't have a local copy
+if [[ ! -d /var/www/html/repositories/${DISTRIBUTION}/${MINOR_RELEASE}/${ARCH}/bluebanquise/ ]]; then
+    (cd /var/www/html/repositories/${DISTRIBUTION}/${MINOR_RELEASE}/${ARCH}/ && /usr/bin/wget -rkp -l2 -np -nH -R "index.html*" -R "*.gif" --cut-dirs=3 https://bluebanquise.com/repository/el${MAJOR_RELEASE}/${ARCH}/bluebanquise/)
+fi
+/usr/sbin/restorecon -Rv /var/www/html/repositories/${DISTRIBUTION}/${MINOR_RELEASE}/${ARCH}/bluebanquise
+
+# Copy the inventory
+/usr/bin/cp -a /etc/bluebanquise/resources/examples/simple_cluster/* /etc/bluebanquise/
+
+# Create software share for NFS
+/usr/bin/mkdir -p /opt/softwares
+
+echo "Generate SSH key pairs"
+/usr/bin/ssh-keygen -f /root/.ssh/id_rsa -N ""
+/usr/bin/cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+
+# Copy public key of the mgmt to the inventory
+/usr/bin/sed -i -e "s#- ssh-rsa.*#- $(cat /root/.ssh/authorized_keys)#" \
+  /etc/bluebanquise/inventory/group_vars/all/all_equipments/authentication.yml
+
+echo "Set the distribution release"
+/usr/bin/sed -i -e "s/distribution_major_version: ./distribution_major_version: ${MAJOR_RELEASE}/" \
+                -e "s/distribution_version: .*/distribution_version: ${MINOR_RELEASE}/" \
+                -e "s/distribution: .*/distribution: ${DISTRIBUTION}/" \
+                /etc/bluebanquise/inventory/group_vars/equipment_typeM/equipment_profile.yml
+/usr/bin/sed -i -e "s/distribution_major_version: ./distribution_major_version: ${MAJOR_RELEASE}/" \
+                -e "s/distribution_version: .*/distribution_version: ${MINOR_RELEASE}/" \
+                -e "s/distribution: .*/distribution: ${DISTRIBUTION}/" \
+                /etc/bluebanquise/inventory/group_vars/equipment_typeC/equipment_profile.yml
+/usr/bin/sed -i -e "s/distribution_major_version: ./distribution_major_version: ${MAJOR_RELEASE}/" \
+                -e "s/distribution_version: .*/distribution_version: ${MINOR_RELEASE}/" \
+                -e "s/distribution: .*/distribution: ${DISTRIBUTION}/" \
+                /etc/bluebanquise/inventory/group_vars/equipment_typeL/equipment_profile.yml
